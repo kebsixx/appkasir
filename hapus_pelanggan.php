88d93912f21887186cb1985c2b6610814fbf40dc
kebsixx <rijikanw@gmail.com>
2025-01-11 13:58:26 +0700

    create delete customer page

---
+++ hapus_pelanggan.php
@@ -0,0 +1,10 @@
+<?php
+$id = $_GET['id'];
+$query = mysqli_query($koneksi, "DELETE FROM pelanggan WHERE PelangganID = '$id'");
+if ($query) {
+    echo "<script>alert('Data Berhasil Dihapus');</script>";
+    echo "<script>location='?page=pelanggan';</script>";
+} else {
+    echo "<script>alert('Data Gagal Dihapus');</script>";
+    echo "<script>location='?page=edit_pelanggan&id=$id';</script>";
+}
