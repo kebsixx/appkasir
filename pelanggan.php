36e1220998c2ef0daa2a96fc79ef0ad09b5f29a8
kebsixx <rijikanw@gmail.com>
2025-01-11 13:49:23 +0700

    create add customer page

--- pelanggan.php
+++ pelanggan.php
@@ -1,7 +1,7 @@
 <div class="container-fluid px-4">
-    <h1 class="mt-4">Dashboard</h1>
+    <h1 class="mt-4">Pelanggan</h1>
     <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Dashboard</li>
+        <li class="breadcrumb-item active">Pelanggan</li>
     </ol>
 
     <a href="?page=tambah_pelanggan" class="btn btn-primary mb-4">Tambah Data</a>
@@ -17,12 +17,12 @@
         while ($pelanggan = mysqli_fetch_array($query)) {
         ?>
             <tr>
-                <td><?= $pelanggan['nama_pelanggan']; ?></td>
-                <td><?= $pelanggan['alamat']; ?></td>
-                <td><?= $pelanggan['nomor_telepon']; ?></td>
+                <td><?= $pelanggan['NamaPelanggan']; ?></td>
+                <td><?= $pelanggan['Alamat']; ?></td>
+                <td><?= $pelanggan['NomorTelepon']; ?></td>
                 <td>
-                    <a href="?page=edit_pelanggan&id=<?= $pelanggan['id_pelanggan']; ?>" class="btn btn-warning">Edit</a>
-                    <a href="?page=hapus_pelanggan&id=<?= $pelanggan['id_pelanggan']; ?>" class="btn btn-danger">Hapus</a>
+                    <a href="?page=edit_pelanggan&id=<?= $pelanggan['PelangganID']; ?>" class="btn btn-warning">Edit</a>
+                    <a href="?page=hapus_pelanggan&id=<?= $pelanggan['PelangganID']; ?>" class="btn btn-danger">Hapus</a>
                 </td>
             </tr>
         <?php } ?>
---
+++ tambah_pelanggan.php
@@ -0,0 +1,45 @@
+<?php
+if (isset($_POST['simpan'])) {
+    $NamaPelanggan = $_POST['NamaPelanggan'];
+    $NomorTelepon = $_POST['NomorTelepon'];
+    $Alamat = $_POST['Alamat'];
+
+    $query = mysqli_query($koneksi, "INSERT INTO pelanggan (NamaPelanggan, Alamat, NomorTelepon) VALUES ('$NamaPelanggan', '$Alamat', '$NomorTelepon')");
+
+    if ($query) {
+        echo "<script>alert('Data berhasil ditambahkan');</script>";
+        echo "<script>location='?page=pelanggan';</script>";
+    } else {
+        echo "<script>alert('Data gagal ditambahkan');</script>";
+        echo "<script>location='?page=tambah_pelanggan';</script>";
+    }
+}
+?>
+
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Pelanggan</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active"><a href="?page=pelanggan">Pelanggan</a> > Tambah Pelanggan</li>
+    </ol>
+
+    <form action="" method="post">
+        <table class="table">
+            <tr>
+                <td>Nama Pelanggan</td>
+                <td><input type="text" name="NamaPelanggan" class="form-control" required></td>
+            </tr>
+            <tr>
+                <td>Nomor Telepon</td>
+                <td><input type="text" name="NomorTelepon" class="form-control" required></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td><textarea name="Alamat" id="alamat" class="form-control"></textarea></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="simpan" value="Simpan" class="btn btn-primary"></td>
+            </tr>
+        </table>
+    </form>
+</div>
