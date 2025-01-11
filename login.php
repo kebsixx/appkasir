273387ba43cbcd849a1b17788adb17df02c87529
kebsixx <rijikanw@gmail.com>
2025-01-11 10:52:46 +0700

    add login script

--- login.php
+++ login.php
@@ -1,3 +1,30 @@
+<?php 
+    include "koneksi.php";
+    if (isset($_POST['username'])) {
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+
+        $cek = mysqli_query($koneksi, "SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+
+        if (mysqli_num_rows($cek) > 0) {
+            $data = mysqli_fetch_array($cek);
+            $_SESSION['user'] = $data;
+            echo `
+                <script>
+                    alert("Selamat datang!");
+                    location.href="index.php";
+                </script>
+            `;
+        } else {
+            echo `
+                <script>
+                    alert('Username/Password salah.');
+                </script>
+            `;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
