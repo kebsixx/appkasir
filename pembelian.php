7f7e613f3901dab4cdd06f63d577b5273e600db4
kebsixx <rijikanw@gmail.com>
2025-01-11 14:28:00 +0700

    create order page

---
+++ pembelian.php
@@ -0,0 +1,30 @@
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Pembelian</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Pembelian</li>
+    </ol>
+
+    <a href="?page=tambah_produk" class="btn btn-primary mb-4">Tambah Data</a>
+    <table class="table table-bordered">
+        <tr>
+            <th>Tanggal Penjualan</th>
+            <th>Pelanggan</th>
+            <th>Total Harga</th>
+            <th>Aksi</th>
+        </tr>
+        <?php
+        $query = mysqli_query($koneksi, "SELECT * FROM penjualan LEFT JOIN pelanggan ON penjualan.PelangganID = pelanggan.PelangganID");
+        while ($pembelian = mysqli_fetch_array($query)) {
+        ?>
+            <tr>
+                <td><?= $pembelian['TanggalPenjualan']; ?></td>
+                <td><?= $pembelian['NamaPelanggan']; ?></td>
+                <td><?= $pembelian['TotalHarga']; ?></td>
+                <td>
+                    <a href="?page=edit_produk&id=<?= $pembelian['PenjualanID']; ?>" class="btn btn-warning">Edit</a>
+                    <a href="?page=hapus_produk&id=<?= $pembelian['PenjualanID']; ?>" class="btn btn-danger">Hapus</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+</div>
